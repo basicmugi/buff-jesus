71411e87298d27ee3321e3afadba1455282cf18b
basicmugi <32236533+basicmugi@users.noreply.github.com>
2018-02-09 23:03:47 +0200

    Add files via upload

---
+++ adam.py
@@ -0,0 +1,53 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+
+Client = discord.Client()
+client = commands.Bot(command_prefix = "?")
+
+@client.event
+async def on_ready():
+    print("hi brona is my gf")
+
+
+@client.command(pass_context=True)
+async def bae(ctx):
+    await client.say("brona")
+
+@client.command(pass_context=True)
+async def kry(ctx):
+    await client.say("is a cheater!!!!")
+
+@client.command(pass_context=True)
+async def eoin(ctx):
+    await client.say("epic gamer nazi")
+
+@client.command(pass_context=True)
+async def belma(ctx):
+    await client.say("super op gamer!!!!!!!! XD")
+
+@client.command(pass_context=True)
+async def gf(ctx):
+    await client.say("brona ofc")
+
+
+@client.command(pass_context=True)
+async def cheater(ctx):
+    await client.say("NEVER")
+
+@client.command(pass_context=True)
+async def mintie(ctx):
+    await client.say("she dumb lole")
+
+@client.command(pass_context=True)
+async def mintieswirl(ctx):
+    await client.say("she dumb lole")
+
+@client.command (pass_context=True)
+async def joke(ctx):
+    await client.say("I'm not loyal to brona")
+    
+
+client.run("NDAwNjk1NDYzMTIyMDQyODgw.DV4VVw.dpCVT4Vw6D4HUJa2BrIbv4H8SUE")                            
